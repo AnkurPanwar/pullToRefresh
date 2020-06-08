87a817f865781840b4c8b4a2e060f95c6107cdb5
Ankur <panwarankur23@gmail.com>
2020-06-08 22:06:06 +0530

    Initial Commit

---
+++ PullToRefresh/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PullToRefresh
+//
+//  Created by Ankur on 08/06/20.
+//  Copyright © 2020 Ankur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
