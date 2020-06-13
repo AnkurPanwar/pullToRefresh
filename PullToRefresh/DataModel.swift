10a646486f17bc68d7fbc88aeb9b670f91e1d518
Ankur <panwarankur23@gmail.com>
2020-06-13 19:51:26 +0530

    pull to refresh

---
+++ PullToRefresh/DataModel.swift
@@ -0,0 +1,15 @@
+//
+//  DataModel.swift
+//  PullToRefresh
+//
+//  Created by Ankur on 08/06/20.
+//  Copyright © 2020 Ankur. All rights reserved.
+//
+
+import Foundation
+
+class DataModel
+{
+    var diceArr: [Int] = [1,3,5,3]
+}
+
--- PullToRefresh/ViewController.swift
+++ PullToRefresh/ViewController.swift
@@ -8,13 +8,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    private let refreshControl = UIRefreshControl()
+    var dataModel: DataModel = DataModel()
+    @IBOutlet private weak var tableView: UITableView!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
+        
+        refreshControl.backgroundColor = .lightGray
+        refreshControl.tintColor = .red
+        let font = UIFont.systemFont(ofSize: 30, weight: .light)
+        let attributes = [NSAttributedString.Key.font: font]
+        refreshControl.attributedTitle = NSAttributedString(string: "Fetching new values...", attributes: attributes)
         // Do any additional setup after loading the view.
+        if #available(iOS 10.0, *) {
+            tableView.refreshControl = refreshControl
+        } else {
+            tableView.addSubview(refreshControl)
+        }
+        
+        refreshControl.addTarget(self, action: #selector(regenerateRandomNumber), for: .valueChanged)
     }
+    
+    @objc private func regenerateRandomNumber(_ sender: Any) {
+        dataModel.diceArr.append(Int.random(in: 1...6))
+        tableView.reloadData()
+        self.refreshControl.endRefreshing()
+    }
+}
 
+extension ViewController: UITableViewDelegate
+{
+    
+}
 
+extension ViewController: UITableViewDataSource
+{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return dataModel.diceArr.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell: RandomCell = tableView.dequeueReusableCell(withIdentifier: "RandomCell") as! RandomCell
+        cell.cellInfo = (indexPath.row + 1 , dataModel.diceArr[indexPath.row])
+        return cell
+    }    
 }
 
